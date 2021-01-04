6b936b40d4e05610f7506c5ec22f91d3f80e5540
ヴォーホアン ロン <vlong@yahoo-corp.jp>
2021-01-04 09:34:36 +0700

    implment DLT consumer

--- downloader/src/main/java/com/realestate/crawler/downloader/consumer/DownloadStarterUrlDLTConsumer.java
+++ downloader/src/main/java/com/realestate/crawler/downloader/consumer/DownloadStarterUrlDLTConsumer.java
@@ -1,9 +1,11 @@
 package com.realestate.crawler.downloader.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.realestate.crawler.downloader.message.DownloadStarterUrlMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +15,18 @@ import java.util.concurrent.ExecutionException;
 @Slf4j
 public class DownloadStarterUrlDLTConsumer {
 
-    @KafkaListener(id = "dltGroup", topics = "download-starter.DLT")
-    public void listen(Object message) throws ExecutionException, InterruptedException {
-        ConsumerRecord consumerRecord = (ConsumerRecord) message;
+    private final ObjectMapper objectMapper;
 
-        DownloadStarterUrlMessage downloadStarterUrlMessage = null;
+    @Autowired
+    public DownloadStarterUrlDLTConsumer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @KafkaListener(id = "dltGroup", topics = "${spring.kafka.topic.downloadStarterDLT}")
+    public void listen(Object message) throws ExecutionException, InterruptedException, JsonProcessingException {
+        ConsumerRecord consumerRecord = (ConsumerRecord) message;
+        DownloadStarterUrlMessage downloadStarterUrlMessage = objectMapper.readValue(consumerRecord.value().toString(), DownloadStarterUrlMessage.class);
+
         log.info("Download starter message DLT: {}", downloadStarterUrlMessage);
     }
 }
